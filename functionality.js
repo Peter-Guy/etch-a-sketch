cc15ffb3a1167a98e8d67e030769805dd7426b91
Peter-Guy <155556339+Peter-Guy@users.noreply.github.com>
2024-04-08 13:41:41 -0400

    Added a simple menu with button grid and erase

--- functionality.js
+++ functionality.js
@@ -1,20 +1,24 @@
 function setBoard(){
-    const button = document.querySelector("#button");
+    const button = document.querySelector("#grid");
     button.onclick = ()=>{
         for (let i = 0; i< 506; i++){
             const board = document.querySelector(".board");
-            let pixel = document.createElement("div");
-            pixel.setAttribute("id", "pixels");
+            const pixel = document.createElement("div");
+            pixel.setAttribute("class", "pixels");
             board.appendChild(pixel);
+            
+            pixel.onmousedown = ("click", (event) =>{
+                changeColor(event.target);
+            })
         }
+    };t
+    const erase = document.getElementById("erase");
+    erase.onclick = () =>{
+        
     }
 }
-function changeColor(){
-    let color = document.getElementById("pixels");
-    color.style.backgroundColor = "#00FF00";
+function changeColor(pixel){
+        let color = "black";
+        pixel.style.backgroundColor = color;
 }
 setBoard();
-const pixeeel = document.getElementById("pixels");
-    pixeeel.onclick = () =>{
-         changeColor();
-    }
